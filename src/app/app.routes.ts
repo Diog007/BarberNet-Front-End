e94fb7ba131f40205003877c45fa97b5c95cc46f
Diog007 <diogo.mz123@gmail.com>
2024-05-28 15:24:06 -0300

    Route to LOGIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    
     {path: '', component: NavComponent},
 
 ];
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router ) { }
 
   ngOnInit(): void {
-    this.router.navigate([''])
+    this.router.navigate(['login'])
   }
 
 }
