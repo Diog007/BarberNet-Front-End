e9b0fa9900864b3d5d8e0b3ac8d618963da1381f
Diog007 <diogo.mz123@gmail.com>
2024-05-28 14:08:56 -0300

    HTML and CSS for nav components

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {  Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatListModule, MatIconModule,RouterOutlet,
+     RouterLink, MatSidenavModule, MatIconModule ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+
+  constructor(private router: Router ) { }
+
+  ngOnInit(): void {
+    this.router.navigate([''])
+  }
 
 }
