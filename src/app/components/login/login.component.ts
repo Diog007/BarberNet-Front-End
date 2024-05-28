47b4c6398d180baeda48b7dca871fa395f22d808
Diog007 <diogo.mz123@gmail.com>
2024-05-28 15:44:00 -0300

    Validation of login forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+import {  HttpClientModule } from '@angular/common/http';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import {  RouterLink } from '@angular/router';
+import { Credenciais } from '../../models/Credenciais';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  ngOnInit(): void {
+  }
+
+  creds: Credenciais  = {
+    login: '',
+    senha: ''
+  };
+
+  login = new FormControl(null, Validators.minLength(3))
+  senha = new FormControl(null, Validators.minLength(3))
+
+
+  validaCampos(): boolean{
+    return this.login.valid && this.senha.valid;
+  }
 
 }
