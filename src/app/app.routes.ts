2d51bf0c914ca834efb2e8bc6e82b885c312a95d
Diog007 <diogo.mz123@gmail.com>
2024-05-29 15:13:21 -0300

    Authentication with token completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    
-    {path: '', component: NavComponent},
+        {path: '', component: NavComponent, canActivate: [authGuard], children: [
+
+
+        ]
+
+    }
 
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,11 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credenciais } from '../models/Credenciais';
 import { API_CONFIG } from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
+
 
   constructor(private http: HttpClient) { }
 
@@ -17,5 +21,13 @@ export class AuthService {
   successfulLogin(authToken: string, login: string) {
     localStorage.setItem('token', authToken)
     localStorage.setItem('email', login)
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if(token != null){
+      return !this.jwtService.isTokenExpired(token);
+    }
+    return false;
   }
 }
