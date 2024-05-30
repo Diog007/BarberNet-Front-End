33580e1591d18ddc969ab88a163f266cdf3da212
Diog007 <diogo.mz123@gmail.com>
2024-05-30 15:16:35 -0300

    Refactoring route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
-import { CabeleireirosComponent } from './components/cabeleireiros/cabeleireiros.component';
+import { CabeleireirosListComponent } from './components/cabeleireiros/cabeleireiros-list/cabeleireiros-list.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
         {path: '', component: NavComponent, canActivate: [authGuard], children: [
             
             {path: 'home', component: HomeComponent},
-            {path: 'cabeleireiros', component: CabeleireirosComponent}
+            {path: 'cabeleireiros', component: CabeleireirosListComponent}
 
         ]
     }
