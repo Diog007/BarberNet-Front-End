186c8be1c4b17b18067f06c22329a11ff352b734
Diog007 <diogo.mz123@gmail.com>
2024-05-30 11:43:13 -0300

    Hairdressers component created

---
+++ src/app/components/cabeleireiros/cabeleireiros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeleireiros',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabeleireiros.component.html',
+  styleUrl: './cabeleireiros.component.css'
+})
+export class CabeleireirosComponent {
+
+}
