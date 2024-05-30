ecddaf248c8a8d70adfbeea5061cb081ab4477b7
Diog007 <diogo.mz123@gmail.com>
2024-05-30 12:04:31 -0300

    Html and CSS for hairdressing components

--- src/app/components/cabeleireiros/cabeleireiros.component.ts
+++ src/app/components/cabeleireiros/cabeleireiros.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import {  MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-cabeleireiros',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule, MatFormFieldModule, MatButtonModule, MatInputModule, MatTableModule, RouterLink],
   templateUrl: './cabeleireiros.component.html',
   styleUrl: './cabeleireiros.component.css'
 })
