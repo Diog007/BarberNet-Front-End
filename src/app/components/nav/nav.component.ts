ad0d286b29db8db91e10a6ccf15bc408022d79fb
Diog007 <diogo.mz123@gmail.com>
2024-05-30 11:31:39 -0300

    Home HTML and CSS

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private serviceAuth: AuthService, private toast: ToastrService  ) { }
 
   ngOnInit(): void {
-    this.router.navigate([''])
+    this.router.navigate(['home'])
   }
 
   logout(): void {
