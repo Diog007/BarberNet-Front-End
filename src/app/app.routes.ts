86dfa17ac525d48671a53b05f6aa4661b3924cfb
Diog007 <diogo.mz123@gmail.com>
2024-05-30 11:31:24 -0300

    Routes for home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
 import { authGuard } from './auth/auth.guard';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
         {path: '', component: NavComponent, canActivate: [authGuard], children: [
-
+            {path: 'home', component: HomeComponent},
 
         ]
 
