98f44ac310b637787285f7b700eacd61ce0129bc
Diog007 <diogo.mz123@gmail.com>
2024-05-31 11:20:10 -0300

    Created hairdresser-create component

---
+++ src/app/components/cabeleireiros/cabeleireiros-create/cabeleireiros-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeleireiros-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabeleireiros-create.component.html',
+  styleUrl: './cabeleireiros-create.component.css'
+})
+export class CabeleireirosCreateComponent {
+
+}
