2e08b42b674122e1d9fc5fcc845847a2e2ce9cdc
Diog007 <diogo.mz123@gmail.com>
2024-05-31 14:35:27 -0300

    HTML and CSS for HAIRDRESSERS-DELETE

--- src/app/components/cabeleireiros/cabeleireiros-delete/cabeleireiros-delete.component.ts
+++ src/app/components/cabeleireiros/cabeleireiros-delete/cabeleireiros-delete.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { RouterLink } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-cabeleireiros-delete',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatPseudoCheckboxModule, FormsModule, MatRadioModule,
+    MatCheckboxModule, MatFormFieldModule, MatButtonModule, 
+    MatInputModule, MatIconModule, RouterLink, ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './cabeleireiros-delete.component.html',
   styleUrl: './cabeleireiros-delete.component.css'
 })
