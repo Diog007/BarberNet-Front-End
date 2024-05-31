bf20850ed0318257667a63c65f6141d75d54f34e
Diog007 <diogo.mz123@gmail.com>
2024-05-31 12:59:11 -0300

    HTML and CSS for HAIRDRESSERS

--- src/app/components/cabeleireiros/cabeleireiros-create/cabeleireiros-create.component.ts
+++ src/app/components/cabeleireiros/cabeleireiros-create/cabeleireiros-create.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-cabeleireiros-create',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatPseudoCheckboxModule, FormsModule, MatRadioModule,
+    MatCheckboxModule, MatFormFieldModule, MatButtonModule, 
+    MatInputModule, MatIconModule, RouterLink, ReactiveFormsModule],
   templateUrl: './cabeleireiros-create.component.html',
   styleUrl: './cabeleireiros-create.component.css'
 })
