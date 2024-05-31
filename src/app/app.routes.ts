d602777ba47b97d0cf9876f5ddd908a60ee94cfd
Diog007 <diogo.mz123@gmail.com>
2024-05-31 14:30:26 -0300

    Routes to delete hairdressers component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { CabeleireirosListComponent } from './components/cabeleireiros/cabeleireiros-list/cabeleireiros-list.component';
 import { CabeleireirosCreateComponent } from './components/cabeleireiros/cabeleireiros-create/cabeleireiros-create.component';
+import { CabeleireirosDeleteComponent } from './components/cabeleireiros/cabeleireiros-delete/cabeleireiros-delete.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -12,7 +13,9 @@ export const routes: Routes = [
             
             {path: 'home', component: HomeComponent},
             {path: 'cabeleireiros', component: CabeleireirosListComponent},
-            {path: 'cabeleireiros/create', component: CabeleireirosCreateComponent}
+            {path: 'cabeleireiros/create', component: CabeleireirosCreateComponent},
+            {path: 'cabeleireiros/delete', component: CabeleireirosDeleteComponent}
+
 
         ]
     }
