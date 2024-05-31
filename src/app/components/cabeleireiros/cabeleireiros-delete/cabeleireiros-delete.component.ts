2bbc7e1596e4ad3cfbb0f168dd4c59432a62759e
Diog007 <diogo.mz123@gmail.com>
2024-05-31 14:24:05 -0300

    Created hairdresser-delete component

---
+++ src/app/components/cabeleireiros/cabeleireiros-delete/cabeleireiros-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeleireiros-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabeleireiros-delete.component.html',
+  styleUrl: './cabeleireiros-delete.component.css'
+})
+export class CabeleireirosDeleteComponent {
+
+}
