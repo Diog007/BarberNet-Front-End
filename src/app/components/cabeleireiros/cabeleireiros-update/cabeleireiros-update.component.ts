868433139b860a714eafa578db3a3df04ffbd649
Diog007 <diogo.mz123@gmail.com>
2024-05-31 21:16:56 -0300

    Created udate hairdressing component

---
+++ src/app/components/cabeleireiros/cabeleireiros-update/cabeleireiros-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeleireiros-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabeleireiros-update.component.html',
+  styleUrl: './cabeleireiros-update.component.css'
+})
+export class CabeleireirosUpdateComponent {
+
+}
