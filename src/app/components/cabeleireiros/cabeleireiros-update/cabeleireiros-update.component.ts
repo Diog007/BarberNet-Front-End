93d83d0b22a1a6dd8cdb4147c8959b7d169064ea
Diog007 <diogo.mz123@gmail.com>
2024-06-01 23:44:00 -0300

    HTML and CSS for update component

--- src/app/components/cabeleireiros/cabeleireiros-update/cabeleireiros-update.component.ts
+++ src/app/components/cabeleireiros/cabeleireiros-update/cabeleireiros-update.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { RouterLink } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-cabeleireiros-update',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatPseudoCheckboxModule, FormsModule, MatRadioModule,
+    MatCheckboxModule, MatFormFieldModule, MatButtonModule, 
+    MatInputModule, MatIconModule, RouterLink, ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './cabeleireiros-update.component.html',
-  styleUrl: './cabeleireiros-update.component.css'
+  styleUrl: './cabeleireiros-update.component.css',
+  providers: [provideNgxMask()]
 })
 export class CabeleireirosUpdateComponent {
 
