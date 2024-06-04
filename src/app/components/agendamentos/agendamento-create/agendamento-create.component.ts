f847cb54d2f9ee828afab82700d1e5e1d042a3a7
Diog007 <diogo.mz123@gmail.com>
2024-06-03 21:31:20 -0300

    Create scheduling component created

---
+++ src/app/components/agendamentos/agendamento-create/agendamento-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamento-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './agendamento-create.component.html',
+  styleUrl: './agendamento-create.component.css'
+})
+export class AgendamentoCreateComponent {
+
+}
