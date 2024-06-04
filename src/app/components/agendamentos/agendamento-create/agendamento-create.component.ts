58c6b7a5e87cf3d0265e001d59ca2e294498a599
Diog007 <diogo.mz123@gmail.com>
2024-06-04 10:39:00 -0300

    Scheduling HTML and CSS

--- src/app/components/agendamentos/agendamento-create/agendamento-create.component.ts
+++ src/app/components/agendamentos/agendamento-create/agendamento-create.component.ts
@@ -1,9 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-agendamento-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule,
+    MatInputModule, MatSelectModule,
+     MatButtonModule, ReactiveFormsModule, 
+     CommonModule, RouterLink],
   templateUrl: './agendamento-create.component.html',
   styleUrl: './agendamento-create.component.css'
 })
