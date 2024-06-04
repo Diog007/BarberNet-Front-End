c88f8aa5755fd60428261a59d2b6694ae1fe57b6
Diog007 <diogo.mz123@gmail.com>
2024-06-04 18:14:36 -0300

    Schedule-update component created

---
+++ src/app/components/agendamentos/agendamento-update/agendamento-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamento-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './agendamento-update.component.html',
+  styleUrl: './agendamento-update.component.css'
+})
+export class AgendamentoUpdateComponent {
+
+}
