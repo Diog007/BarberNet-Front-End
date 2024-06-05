0aa39dde4b7728ec98a3d52fccd73fc67ab41297
Diog007 <diogo.mz123@gmail.com>
2024-06-05 17:34:48 -0300

    Created read scheduling component

---
+++ src/app/components/agendamentos/agendamento-read/agendamento-read.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agendamento-read',
+  standalone: true,
+  imports: [],
+  templateUrl: './agendamento-read.component.html',
+  styleUrl: './agendamento-read.component.css'
+})
+export class AgendamentoReadComponent {
+
+}
