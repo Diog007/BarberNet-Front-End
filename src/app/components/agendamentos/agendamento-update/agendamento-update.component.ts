bb45be0ab9b459b0b32ec926c21de41393f5f3bd
Diog007 <diogo.mz123@gmail.com>
2024-06-05 11:07:56 -0300

    HTML and CSS for the update scheduling component

--- src/app/components/agendamentos/agendamento-update/agendamento-update.component.ts
+++ src/app/components/agendamentos/agendamento-update/agendamento-update.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterLink } from '@angular/router';
+import { MatDatetimepickerModule, MatNativeDatetimeModule } from '@mat-datetimepicker/core';
 
 @Component({
   selector: 'app-agendamento-update',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule,
+    MatInputModule, MatSelectModule,
+     MatButtonModule, ReactiveFormsModule, 
+     CommonModule, RouterLink,  MatDatetimepickerModule, MatNativeDatetimeModule],
   templateUrl: './agendamento-update.component.html',
   styleUrl: './agendamento-update.component.css'
 })
