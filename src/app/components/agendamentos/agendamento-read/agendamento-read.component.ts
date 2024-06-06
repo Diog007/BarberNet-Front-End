3b24cc94f67d42981fc4fd7a82848ca11e911920
Diog007 <diogo.mz123@gmail.com>
2024-06-06 14:54:35 -0300

    Finished read component

--- src/app/components/agendamentos/agendamento-read/agendamento-read.component.ts
+++ src/app/components/agendamentos/agendamento-read/agendamento-read.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MatDatetimepickerModule, MatNativeDatetimeModule } from '@mat-datetimepicker/core';
+import { Agendamentos } from '../../../models/Agendamentos';
+import { AgendamentoService } from '../../../services/agendamento.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
-  selector: 'app-agendamento-read',
-  standalone: true,
-  imports: [],
-  templateUrl: './agendamento-read.component.html',
-  styleUrl: './agendamento-read.component.css'
+selector: 'app-agendamento-read',
+standalone: true,
+imports: [MatFormFieldModule, FormsModule,
+MatInputModule, MatSelectModule,
+MatButtonModule, RouterLink 
+],
+templateUrl: './agendamento-read.component.html',
+styleUrl: './agendamento-read.component.css'
 })
-export class AgendamentoReadComponent {
+export class AgendamentoReadComponent implements OnInit{
+
+ngOnInit(): void {
+  this.agendamento.id = this.route.snapshot.paramMap.get('id');
+  this.findById();
+}
+       
+    agendamento: Agendamentos = {
+    id: '',
+    cabeleireiro: '',
+    cliente: '',
+    nomeCabeleireiro: '',
+    nomeCliente: '',
+    criacao: '',
+    data: '',
+    precoEstimado: '',
+    statusAgendamento: '',
+    metodoPagamento: '',
+  }
+
+  constructor(
+      private agendamentoService: AgendamentoService,
+      private toastService: ToastrService,
+      private route: ActivatedRoute 
+    ) { }
+
+    findById(): void {
+      this.agendamentoService.findById(this.agendamento.id).subscribe(resp => {
+        this.agendamento = resp;
+      }, ex => {
+        this.toastService.error(ex.error.error);
+      })
+    }
+
 
 }
