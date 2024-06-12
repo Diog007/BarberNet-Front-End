cb34e9c21614819e411e8fe917e84d4d1cf85db2
Diog007 <diogo.mz123@gmail.com>
2024-06-12 11:16:38 -0300

    historic hairdressing component created

---
+++ src/app/components/cabeleireiros/cabeleireiros-historic/cabeleireiros-historic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabeleireiros-historic',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabeleireiros-historic.component.html',
+  styleUrl: './cabeleireiros-historic.component.css'
+})
+export class CabeleireirosHistoricComponent {
+
+}
