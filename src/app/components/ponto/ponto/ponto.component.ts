711eaf237b1c89dc1f9456f3c1764e4be6ead911
Diog007 <diogo.mz123@gmail.com>
2024-06-13 12:36:14 -0300

    Point component created

---
+++ src/app/components/ponto/ponto/ponto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ponto',
+  standalone: true,
+  imports: [],
+  templateUrl: './ponto.component.html',
+  styleUrl: './ponto.component.css'
+})
+export class PontoComponent {
+
+}
