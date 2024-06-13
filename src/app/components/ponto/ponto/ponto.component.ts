ced9866fbad3304742788d85c6e0209c27079313
Diog007 <diogo.mz123@gmail.com>
2024-06-13 12:56:51 -0300

    HTML and CSS for Ponto

--- src/app/components/ponto/ponto/ponto.component.ts
+++ src/app/components/ponto/ponto/ponto.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ponto',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule, MatFormFieldModule, MatButtonModule, MatInputModule, MatTableModule, RouterLink],
   templateUrl: './ponto.component.html',
   styleUrl: './ponto.component.css'
 })
